2625a9233708198774b57fba1eb7c6e9b068b1bc
root <root@debian>
2015-07-01 21:20:24 +0200

    Mehr Dropdowns, Input und Pruefung

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,40 +1,26 @@
 (function() {
   var app = angular.module('dropdownInput', []);
 
-  app.controller('dropdown', ['$scope','$http', function($scope, $http){
+    app.controller('dropdown', ['$scope','$http', function($scope, $http){
       
-    $scope.genders = [];
+    /*$scope.genders = [];
 
     $http.get('http://angular.local:3000/genders.json').
         success(function(data, status, headers, config) {
             $scope.genders = data;
+            //console.log(data);
+            $scope.myGender = $scope.genders[0];
 
         }).
         error(function(data, status, headers, config) {
             alert('Error: Please debug in developer console');
-        });
-
-    $scope.myGender = $scope.genders[0];
+        }); */
   
-//        $scope.getGenders = function(){
-//            $http.get('http://192.168.178.33:3000/genders.json').
-//                success(function(data, status, headers, config) {
-//                    data.forEach(function(id, name) {
-//                      console.log(name);
-//                    });
-//
-//                }).
-//                error(function(data, status, headers, config) {
-//                    alert('Error: Please debug in developer console');
-//                });
-//            };
-//        $scope.myGender = $scope.getGenders[0];
-
-        /*$scope.genders = [
+        $scope.genders = [
             {name: 'Frau'},
             {name: 'Herr'}
         ];
-        $scope.myGender = $scope.genders[0];*/
+        $scope.myGender = $scope.genders[0];
             
         $scope.titels = [
             {name: 'Dr.'},
@@ -51,7 +37,53 @@
             {name: 'Penny'}
         ];
         $scope.myVendor = $scope.vendors[0];
+        
+        $scope.countries = [
+            {name: 'Deutschland'}
+        ];
+        $scope.myCountry = $scope.countries[0];        
+        
+        $scope.states = [
+            {name: 'Baden-Württemberg'},
+            {name: 'Bayern'},
+            {name: 'Berlin'},
+            {name: 'Brandenburg'},
+            {name: 'Bremen'},
+            {name: 'Hamburg'},
+            {name: 'Hessen'},
+            {name: 'Mecklenburg-Vorpommern'},
+            {name: 'Niedersachsen'},
+            {name: 'Nordrhein-Westfalen'},
+            {name: 'Rheinland-Pfalz'},
+            {name: 'Saarland'},
+            {name: 'Sachsen'},
+            {name: 'Sachsen-Anhalt'},
+            {name: 'Schleswig-Holstein'},
+            {name: 'Thüringen'}
+        ];
+        $scope.myState = $scope.states[0];  
       
     }]);
-		
+
+    app.controller('input', ['$scope', function($scope){ 
+    
+    $scope.register = { firstName: '', 
+                        lastName: '',
+                        userName: '',
+                        streetName: '',
+                        streetNumber: '',
+                        zip: '',
+                        cityName: '',
+                        birthDay: new Date(),
+                        birthCity: '',
+                        mailAddress: '',
+                        phoneNumber: '',
+                        mobileNumber: '',
+                        password: '',
+                        repassword: '',
+                        word: /^\s*[a-zäöüÄÖÜß]*\s*$/i
+                      };
+        
+    }]);
+    
 })();
