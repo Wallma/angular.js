37c7f576a70666cb2c6dd28cd1368f37cd23c355
marcel <root@localhost>
2015-08-05 21:39:09 +0200

    some validation

--- Frontend/reg/app.js
+++ Frontend/reg/app.js
@@ -14,33 +14,33 @@
                 alert('Error: Please debug in developer console');
             });
         
-        $scope.firstName = {name: 'firstName', label: 'Vorname', type: 'text', placeholder: 'John', word: /[a-zäöüÄÖÜß\s]*/i, value: ''};
+        $scope.firstName = {name: 'firstName', label: 'Vorname', type: 'text', placeholder: 'John', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.lastName = {name: 'lastName', label: 'Nachname', type: 'text', placeholder: 'Doe', word: /[a-zäöüÄÖÜß\s]*/i, value: ''};
+        $scope.lastName = {name: 'lastName', label: 'Nachname', type: 'text', placeholder: 'Doe', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.userName = {name: 'userName', label: 'Benutzername', type: 'text', placeholder: 'DoeJ', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.userName = {name: 'userName', label: 'Benutzername', type: 'text', placeholder: 'DoeJ', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.streetName = {name: 'streetName', label: 'Straße', type: 'text', placeholder: 'Kingstr.', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.streetName = {name: 'streetName', label: 'Straße', type: 'text', placeholder: 'Kingstr.', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.streetNumber = {name: 'streetNumber', label: 'Hausnummer', type: 'number', placeholder: '12', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.streetNumber = {name: 'streetNumber', label: 'Hausnummer', type: 'number', placeholder: '12', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.zip = {name: 'zip', label: 'Postleitzahl', type: 'number', placeholder: '34003', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.zip = {name: 'zip', label: 'Postleitzahl', type: 'number', placeholder: '34003', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.cityName = {name: 'cityName', label: 'Ort', type: 'text', placeholder: 'Bristol', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.cityName = {name: 'cityName', label: 'Ort', type: 'text', placeholder: 'Bristol', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.birthDay = {name: 'birthDay', label: 'Geburtstag', type: 'date', placeholder: 'dd.MM.yyyy', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.birthDay = {name: 'birthDay', label: 'Geburtstag', type: 'date', placeholder: 'dd.MM.yyyy', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.birthCity = {name: 'birthCity', label: 'Geburtsort', type: 'text', placeholder: 'London', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.birthCity = {name: 'birthCity', label: 'Geburtsort', type: 'text', placeholder: 'London', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.mailAddress = {name: 'mailAddress', label: 'E-Mailadresse', type: 'email', placeholder: 'john.doe@online.de', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.mailAddress = {name: 'mailAddress', label: 'E-Mailadresse', type: 'email', placeholder: 'john.doe@online.de', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.phoneNumber = {name: 'phoneNumber', label: 'Telefonnummer', type: 'tel', placeholder: '80489/0000000', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.phoneNumber = {name: 'phoneNumber', label: 'Telefonnummer', type: 'tel', placeholder: '80489/0000000', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.mobileNumber = {name: 'mobileNumber', label: 'Handynummer', type: 'tel', placeholder: '0172/0000000', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.mobileNumber = {name: 'mobileNumber', label: 'Handynummer', type: 'tel', placeholder: '0172/0000000', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.password = {name: 'password', label: 'Passwort', type: 'password', placeholder: 'password', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.password = {name: 'password', label: 'Passwort', type: 'password', placeholder: 'password', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
         
-        $scope.repassword = {name: 'repassword', label: 'Passwort wiederholen', type: 'password', placeholder: 'password', word: /^\s*[a-zäöüÄÖÜß]*\s*$/i, value: ''};
+        $scope.repassword = {name: 'repassword', label: 'Passwort wiederholen', type: 'password', placeholder: 'password', word: /^[a-zäöüÄÖÜß\s]*$/i, value: ''};
    
         // Validierung:
         $scope.getItemState = function (item)
